3972e4b69c417b50b44915203efb4b2549d51d82
Diego Vilhalva <diegorocha450@yahoo.com>
2024-11-07 11:26:23 -0300

    sidebar created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,15 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
+			dark:{
+				1 :'#1C1F2E',
+				2 :'#161925',
+				3 :'#252A41',
+				4 :'#1E2757'
+			},
+			blue:{
+				1: '#0E78F9'
+			},
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
