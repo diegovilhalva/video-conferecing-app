61d79b2887f3a9ccb2d9779a88d233469d806d07
Diego Vilhalva <diegorocha450@yahoo.com>
2024-11-08 09:05:17 -0300

    custom sign-in and sign-up page created

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from '@clerk/nextjs'
+
+
+const SingUpPage = () => {
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+        <SignUp />
+    </main>
+  )
+}
+
+export default SingUpPage
