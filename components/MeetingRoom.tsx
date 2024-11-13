445020b23de42de8748dd29c403dc24d6baec0df
Diego Vilhalva <diegorocha450@yahoo.com>
2024-11-13 09:36:58 -0300

    personal meeting room  created

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { LayoutList, Users } from 'lucide-react'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import EndCallButton from './EndCallButton'
 import Loader from './Loader'
 
@@ -23,6 +23,7 @@ const MeetingRoom = () => {
   const [showParticipants, setShowParticipants] = useState(false)
   const {useCallCallingState} = useCallStateHooks()
   const callinState = useCallCallingState()
+  const router = useRouter()
   if(callinState !== CallingState.JOINED) return <Loader />
   const CallLayout = () => {
     switch (layout) {
@@ -45,7 +46,7 @@ const MeetingRoom = () => {
         </div>
       </div>
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5  flex-wrap">
-        <CallControls />
+        <CallControls  onLeave={() => router.push('/')} />
         <DropdownMenu>
           <div className="flex items-center">
             <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
